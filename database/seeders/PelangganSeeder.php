95e9433aca71d04e4a9927823a0c683c29e95b0f
Hoki <94770162+Hoki03@users.noreply.github.com>
2023-08-23 13:01:13 +0700

    f

---
+++ database/seeders/PelangganSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Pelanggan;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PelangganSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Pelanggan::create([
+            'nama'          => 'Hoki',
+            'nominal'       => '10000',
+            'keterangan'    => 'Oke',
+            'time'          => '08/23/2023 12:55 PM',
+            'pilihan'       => 'Radio',
+        ]);
+    }
+}
