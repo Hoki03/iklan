0f4ada500bc7efd38eca5b02bd9c4824035d897a
Hoki <94770162+Hoki03@users.noreply.github.com>
2023-08-28 14:42:09 +0700

    d

--- resources/views/print/printall.blade.php
+++ resources/views/print/printall.blade.php
@@ -25,71 +25,72 @@
                     </h2>
                 </div>
             </div> -->
-            <!-- info row -->
-            <div class="row invoice-info">
-                <div class="col-sm-9 invoice-col items-center justufy-center">
-                    <center>
-                    <h2>DINAS KOMUNIKASI DAN INFORMATIKA</h2><br>
-                    <h1><b><u>KOTA SALATIGA</u></b></h1><br>
-                    Jl. Letjend Sukowati No. 51 Salatiga, Jawa Tengah 50724<br>
-                    Telp. :(0298) 326767, Fax : (0298) 321398<br>
-                    Email : diskominfo@salatiga.go.id<br>
-                    Website : https://diskominfo.salatiga.go.id/
-                    </center>
-                </div>
-                <!-- /.col -->
-                <div class="col-sm-3 invoice-col">
-                    <h2><b>BULAN</b></h2><br>
-                    <b>........ </b>
-                </div>
-                <!-- /.col -->
-            </div>
-            <!-- /.row -->
+    <!-- info row -->
+    <div class="row invoice-info">
+        <div class="col-sm-9 invoice-col items-center justufy-center">
+            <center>
+                <h2>DINAS KOMUNIKASI DAN INFORMATIKA</h2><br>
+                <h1><b><u>KOTA SALATIGA</u></b></h1><br>
+                Jl. Letjend Sukowati No. 51 Salatiga, Jawa Tengah 50724<br>
+                Telp. :(0298) 326767, Fax : (0298) 321398<br>
+                Email : diskominfo@salatiga.go.id<br>
+                Website : https://diskominfo.salatiga.go.id/
+            </center>
+        </div>
+        <!-- /.col -->
+        <div class="col-sm-3 invoice-col">
+            <h2><b>BULAN</b></h2><br>
+            <b>........ </b>
+        </div>
+        <!-- /.col -->
+    </div>
+    <!-- /.row -->
 
-            <div class="card-body table-responsive p-0">
-                <table id="table_pelanggan" class="table table-head-fixed text-nowrap">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Nama</th>
-                            <th>Nominal</th>
-                            <th>Keterangan</th>
-                            <th>Waktu</th>
-                            <th>Pilihan</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($data_pelanggan as $d) : ?>
-                        <tr>
-                            <td><?= $d['id'] ?></td>
-                            <td><?= $d['nama']; ?></td>
-                            <td>Rp. <?= number_format($d['nominal'], 0, ',', '.'); ?></td>
-                            <td><?= $d['keterangan']; ?></td>
-                            <td><?= $d['time']; ?></td>
-                            <td><?= $d['pilihan']; ?></td>                    
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+    <div class="card-body table-responsive p-0">
+        <table id="table_pelanggan" class="table table-head-fixed text-nowrap">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nama</th>
+                    <th>Nominal</th>
+                    <th>Keterangan</th>
+                    <th>Waktu</th>
+                    <th>Pilihan</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($data_pelanggan as $d) : ?>
+                    <tr>
+                        <td><?= $d['id'] ?></td>
+                        <td><?= $d['nama']; ?></td>
+                        <td>Rp. <?= number_format($d['nominal'], 0, ',', '.'); ?></td>
+                        <td><?= $d['keterangan']; ?></td>
+                        <td><?= $d['time']; ?></td>
+                        <td><?= $d['pilihan']; ?></td>
+                    </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
+    </div>
 
-            <div class="row">
-                <!-- accepted payments column -->
-                <div class="col-9">
-                </div>
-                <!-- /.col -->
-                <div class="col-3">
-                    <p>Salatiga, 2/22/2014</p>
-                    <br>
-                    <br>
-                    (................................)
-                    </div>
-                </div>
-                <!-- /.col -->
-            </div>
-            <!-- /.row -->
-        </section>
-        <!-- /.content -->
+    <div class="row">
+        <!-- accepted payments column -->
+        <div class="col-9">
+        </div>
+        <!-- /.col -->
+        <div class="col-3">
+            <p>Salatiga, 2/22/2014</p>
+            <br>
+            <br>
+            (................................)
+        </div>
+    </div>
+    <!-- /.col -->
+    </div>
+    <!-- /.row -->
+    </section>
+    <!-- /.content -->
     </div>
     <!-- ./wrapper -->
     <!-- Page specific script -->
