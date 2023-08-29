f194d07feb2e75e7f830a9bc1330014a70cc9c6c
Hoki <94770162+Hoki03@users.noreply.github.com>
2023-08-29 10:23:51 +0700

    c

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -25,7 +25,7 @@
   </svg>
 
   <div class="flex items-center min-h-screen p-6 bg-gray-50 white:bg-gray-900">
-    <div class="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
+    <div class="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg  dark:bg-gray-800">
       <!-- <div class="flex flex-col overflow-y-auto md:flex-row"> -->
       <!-- <div class="h-32 md:h-auto md:w-1/2">
           <img aria-hidden="true" class="object-cover w-full h-full dark:hidden" src="{{asset('windmil/public/assets/img/login-office.jpeg')}}" alt="Office" />
