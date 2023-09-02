59d23e771517227486d968839ee553e7d50e839b
Hoki <94770162+Hoki03@users.noreply.github.com>
2023-09-02 19:49:00 +0700

    c

--- resources/views/layout/dashboard2.blade.php
+++ resources/views/layout/dashboard2.blade.php
@@ -6,9 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Dashboard</title>
     <link rel="stylesheet" href="{{asset('lte/plugins/fontawesome-free/css/all.min.css')}}">
-    <link rel="stylesheet" href="{{asset('lte/plugins/fontawesome-free/css/all.min.css')}}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <link href="{{asset('bootstrap\css\bootstrap.css')}}" rel="stylesheet">
+    <script src="{{asset('bootstrap\js\bootstrap.js')}}"></script>
 
     <link rel="stylesheet" href="{{asset('plt/assets/vendor/css/core.css')}}" class="template-customizer-core-css" />
     <link rel="stylesheet" href="{{asset('plt/assets/vendor/css/theme-default.css')}}" class="template-customizer-theme-css" />
@@ -23,29 +22,24 @@
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+            <div class="container-fluid">
+                <a class="navbar-brand" href="dashboard">
+                    <img src="{{asset('Image\Logo.svg')}}" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+                    Dashboard
+                </a>
+            </div>
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
+                        <button type="button" class="btn btn-outline-secondary"><i class="fas fa-plus"></i></button>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown link
+                        <a class="nav-link dropdown-toggle btn-outline-secondary" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src="{{asset('Image/Vector.svg')}}">
                         </a>
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            <li><a class="dropdown-item" href="edit_user/1">Profile</a></li>
+                            <li><a class="dropdown-item" href="{{route('logout')}}">Logout</a></li>
                         </ul>
                     </li>
                 </ul>
@@ -72,43 +66,43 @@
                     <div data-i18n="Analytics">Riwayat</div>
                 </a>
             </li>
-            <li class="menu-item">
-                <a href="javascript:void(0);" class="menu-link menu-toggle">
-                    <i class="menu-icon tf-icons bx bx-layout"></i>
-                    <div data-i18n="Layouts">Layouts</div>
-                </a>
 
-                <ul class="menu-sub">
-                    <li class="menu-item">
-                        <a href="layouts-without-menu.html" class="menu-link">
-                            <div data-i18n="Without menu">Without menu</div>
-                        </a>
-                    </li>
-                    <li class="menu-item">
-                        <a href="layouts-without-navbar.html" class="menu-link">
-                            <div data-i18n="Without navbar">Without navbar</div>
-                        </a>
-                    </li>
-                    <li class="menu-item">
-                        <a href="layouts-container.html" class="menu-link">
-                            <div data-i18n="Container">Container</div>
-                        </a>
-                    </li>
-                    <li class="menu-item">
-                        <a href="layouts-fluid.html" class="menu-link">
-                            <div data-i18n="Fluid">Fluid</div>
-                        </a>
-                    </li>
-                    <li class="menu-item">
-                        <a href="layouts-blank.html" class="menu-link">
-                            <div data-i18n="Blank">Blank</div>
-                        </a>
-                    </li>
-                </ul>
-            </li>
 
         </ul>
     </aside>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td colspan="2">Larry the Bird</td>
+                <td>@twitter</td>
+            </tr>
+        </tbody>
+    </table>
+
+
+
     <!-- Core JS -->
     <!-- build:js assets/vendor/js/core.js -->
     <script src="{{asset('plt/assets/vendor/libs/jquery/jquery.js')}}"></script>
