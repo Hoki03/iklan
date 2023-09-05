358d8611ee3130b13585a19249601d02e24b347a
Ananda220 <672020220@student.uksw.edu>
2023-09-05 10:09:43 +0700

    re

--- resources/views/layout/admin/table_transaksi.blade.php
+++ resources/views/layout/admin/table_transaksi.blade.php
@@ -223,6 +223,7 @@
                                                             <div class="modal-header">
                                                                 <h4 class="modal-title">Konfirmasi Hapus Data</h4>
                                                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                    <span aria-hidden="true">&times;</span>
                                                                 </button>
                                                             </div>
                                                             <div class="modal-body">
