b367e1781c304fe0491a8b094b48806778003a6e
Hoki <94770162+Hoki03@users.noreply.github.com>
2023-09-05 14:40:28 +0700

    b

--- resources/views/layout/operator/table_transaksi.blade.php
+++ resources/views/layout/operator/table_transaksi.blade.php
@@ -253,34 +253,10 @@
                                                         ?></td>
                                                     <td>
 
-                                                        <a href="{{route('admin.struk',['id'=>$d->id])}}" rel="noopener" target="_blank" class="btn btn-default"><i class="fas fa-print"></i> Print</a>
+                                                        <a href="{{route('operator.struk',['id'=>$d->id])}}" rel="noopener" target="_blank" class="btn btn-default"><i class="fas fa-print"></i> Print</a>
                                                     </td>
                                                 </tr>
-                                                <div class="modal fade" id="modal-hapus{{$d->id}}">
-                                                    <div class="modal-dialog">
-                                                        <div class="modal-content">
-                                                            <div class="modal-header">
-                                                                <h4 class="modal-title">Konfirmasi Hapus Data</h4>
-                                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                                    <span aria-hidden="true">&times;</span>
-                                                                </button>
-                                                            </div>
-                                                            <div class="modal-body">
-                                                                <p>Apakah kamu ingin menghapus data ini? (<b>{{$d->nama}}</b></b>)</p>
-                                                            </div>
-                                                            <div class="modal-footer content-between">
-                                                                <form action="{{route('admin.hapus_transaksi',['id'=>$d->id])}}" method="POST">
-                                                                    @csrf
-                                                                    @method('DELETE')
-                                                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                                                    <button type="submit" class="btn btn-primary">Hapus</button>
-                                                                </form>
-                                                            </div>
-                                                        </div>
-                                                        <!-- /.modal-content -->
-                                                    </div>
-                                                    <!-- /.modal-dialog -->
-                                                </div>
+
                                                 <!-- /.modal -->
                                             <?php endforeach; ?>
                                         </tbody>
