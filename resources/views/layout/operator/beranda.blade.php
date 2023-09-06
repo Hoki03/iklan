41da770b1ce45db3b57958688746315345769dab
Tiara Syah <tiarasyahip@gmail.com>
2023-09-06 17:44:10 +0700

    Memperbarui beranda operator - Mengganti tulisan admin menjadi operator pada beranda

--- resources/views/layout/operator/beranda.blade.php
+++ resources/views/layout/operator/beranda.blade.php
@@ -78,7 +78,7 @@
                         <img src="{{asset('Image/logo-kominfo-transparent.png')}}" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
-                        <a href="#" class="d-block">Admin Kominfo</a>
+                        <a href="#" class="d-block">Operator Kominfo</a>
                     </div>
                 </div>
 
