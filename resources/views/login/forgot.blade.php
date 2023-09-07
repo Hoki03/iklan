3ef73fc02b05617cec1dc8de4201e8d46c908e31
Tiara Syah <tiarasyahip@gmail.com>
2023-09-07 18:24:36 +0700

    Memperbarui login dan forgot

--- resources/views/login/forgot.blade.php
+++ resources/views/login/forgot.blade.php
@@ -14,10 +14,20 @@
   <link rel="stylesheet" href="{{asset('lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
   <!-- Theme style -->
   <link rel="stylesheet" href="{{asset('lte/dist/css/adminlte.min.css')}}">
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+  <link rel="stylesheet" href="{{asset('windmil/public/assets/css/tailwind.output.css')}}" />
+  <link rel="stylesheet" type="text/css" href="{{asset('assets/css/login.css')}}" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer>
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="{{asset('windmil/public/assets/js/init-alpine.js')}}"></script>
+
 </head>
 
 <body class="hold-transition login-page">
-  <div class="login-box">
+  <!-- /lupa password -->
+  <!-- <div class="login-box">
     <div class="card card-outline card-primary">
       <div class="card-header text-center">
         <a href="{{asset('/')}}" class="h1"><b>Password Reset</a>
@@ -35,23 +45,22 @@
           </div>
           <div class="row">
             <div class="col-12">
-              <button type="submit" class="btn btn-primary btn-block">Minta</button>
+              <button type="submit" class="btn btn-primary btn-block">Kirim</button>
             </div>
-            <!-- /.col -->
           </div>
         </form>
       </div>
-      <!-- /.login-card-body -->
     </div>
-  </div>
-  <!-- /.login-box -->
+  </div> -->
 
-  <!-- jQuery -->
-  <script src="{{asset('lte/plugins/jquery/jquery.min.js')}}"></script>
-  <!-- Bootstrap 4 -->
-  <script src="{{asset('lte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-  <!-- AdminLTE App -->
-  <script src="{{asset('lte/dist/js/adminlte.min.js')}}"></script>
+  
+
+    <!-- jQuery -->
+    <script src="{{asset('lte/plugins/jquery/jquery.min.js')}}"></script>
+    <!-- Bootstrap 4 -->
+    <script src="{{asset('lte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <!-- AdminLTE App -->
+    <script src="{{asset('lte/dist/js/adminlte.min.js')}}"></script>
 </body>
 
 </html>
